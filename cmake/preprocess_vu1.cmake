a3587e5238891f89e529dd04380440a32ee7f3d7
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2025-12-01 17:36:10 +0100

    Migrating to `cmake`

---
+++ cmake/preprocess_vu1.cmake
@@ -0,0 +1,49 @@
+# VU1 preprocessing script
+# Usage: cmake -D INPUT=<input> -D OUTPUT=<output> -D STEP=<step> -D SOURCE_DIR=<dir> -D COMPILER=<cc> -D MEM_HEADER=<header> -P preprocess_vu1.cmake
+
+if(STEP STREQUAL "pp1")
+    # Step 1: Remove #include, #define, fix .include paths
+    execute_process(
+        COMMAND /bin/bash -c "cat ${INPUT} | sed -E 's/#include[[:space:]]+.+// ; s/#define[[:space:]]+.+// ; s|(\\.include[[:space:]]+)\\\"([^/].+)\\\"|\\1\\\"${SOURCE_DIR}/vu1/\\2\\\"|' > ${OUTPUT}"
+        RESULT_VARIABLE result
+    )
+    if(NOT result EQUAL 0)
+        message(FATAL_ERROR "Step 1 preprocessing failed")
+    endif()
+
+elseif(STEP STREQUAL "pp2")
+    # Step 2: gasp/masp preprocessor
+    if(NOT DEFINED GASP_TOOL)
+        message(FATAL_ERROR "GASP_TOOL not defined")
+    endif()
+    execute_process(
+        COMMAND ${GASP_TOOL} -c ";" -I${SOURCE_DIR}/vu1 -o ${OUTPUT} ${INPUT}
+        RESULT_VARIABLE result
+    )
+    if(NOT result EQUAL 0)
+        message(FATAL_ERROR "Step 2 preprocessing (${GASP_TOOL}) failed")
+    endif()
+
+elseif(STEP STREQUAL "pp3")
+    # Step 3: Array notation conversion
+    execute_process(
+        COMMAND /bin/bash -c "cat ${INPUT} | sed -E 's/\\[([0-9])\\]/_\\1/g ; s/\\[([w-zW-Z])\\]/\\1/g' > ${OUTPUT}"
+        RESULT_VARIABLE result
+    )
+    if(NOT result EQUAL 0)
+        message(FATAL_ERROR "Step 3 preprocessing failed")
+    endif()
+
+elseif(STEP STREQUAL "pp4")
+    # Step 4: C preprocessor with memory layout
+    execute_process(
+        COMMAND /bin/bash -c "cat ${INPUT} | ${COMPILER} -E -P -I${SOURCE_DIR}/vu1 -imacros ${MEM_HEADER} -o ${OUTPUT} -"
+        RESULT_VARIABLE result
+    )
+    if(NOT result EQUAL 0)
+        message(FATAL_ERROR "Step 4 preprocessing failed")
+    endif()
+
+else()
+    message(FATAL_ERROR "Unknown step: ${STEP}")
+endif()
